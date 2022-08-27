8b3f81561114c52969250f6880a0e0fc8dd94ebb
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-27 17:15:03 +0200

    Test Using Manual Dependency Injection

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -0,0 +1,52 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class DefaultTasksRepositoryTest {
+    private val task1 = Task("Title1", "Description1")
+    private val task2 = Task("Title2", "Description2")
+    private val task3 = Task("Title3", "Description3")
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
+    private val localTasks = listOf(task3).sortedBy { it.id }
+    private val newTasks = listOf(task3).sortedBy { it.id }
+
+    private lateinit var tasksRemoteDataSource: FakeDataSource
+    private lateinit var tasksLocalDataSource: FakeDataSource
+
+    // Class under test
+    private lateinit var tasksRepository: DefaultTasksRepository
+
+    @Before
+    fun createRepository() {
+        tasksRemoteDataSource = FakeDataSource(remoteTasks.toMutableList())
+        tasksLocalDataSource = FakeDataSource(localTasks.toMutableList())
+
+        tasksRepository = DefaultTasksRepository(
+            tasksRemoteDataSource,
+            tasksLocalDataSource,
+            Dispatchers.Unconfined // should be main thread
+        )
+
+    }
+
+    @Test
+    fun getTasks_requestsAllTasksFromRemoteDataSource() = runBlockingTest {
+        // When tasks are requested from the tasks repository
+        val tasks = tasksRepository.getTasks(true) as Result.Success
+
+        // Then tasks are loaded from the remote data source
+        assertThat(tasks.data).isEqualTo(localTasks)
+    }
+}
+// To run your tests, use the function runBlockingTest. This is a function provided by the coroutines test library.
+// It takes in a block of code and then runs this block of code in a special coroutine context
+// which runs synchronously and immediately, meaning actions will occur in a deterministic order.
+// This essentially makes your coroutines run like non-coroutines, so it is meant for testing code.
